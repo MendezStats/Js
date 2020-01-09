7dda37edbb73b387a983818d91c51ee45881757f
MendezStats <59672359+MendezStats@users.noreply.github.com>
2020-01-08 20:23:20 -0400

    Add files via upload

---
+++ MyJs.js
@@ -0,0 +1,13 @@
+
+
+function inicio(){
+
+for(i=0;i<$('.KambiBC-mod-outcome').length;i++){
+
+if(parseFloat($('.KambiBC-mod-outcome')[i].textContent.substr($('.KambiBC-mod-outcome')[i].textContent.length-4,$('.KambiBC-mod-outcome')[i].textContent.length-1))<=1.20 && parseFloat($('.KambiBC-mod-outcome')[i].textContent.substr($('.KambiBC-mod-outcome')[i].textContent.length-4,$('.KambiBC-mod-outcome')[i].textContent.length-1))>=1.01  ){
+
+$('.KambiBC-mod-outcome')[i].click()
+break
+
+
+}}}
